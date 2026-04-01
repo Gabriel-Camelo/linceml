36b0c9f3ad8ae736ede1514bc0dd0d27425a35e7
Gabriel Camelo <gabrielcamelo.dev@gmail.com>
2026-04-01 20:18:39 -0300

    Sprint 01: Initializing project development and add report schema (#5)
    * feat(report): add report schema
    
    * docs(init): add init module docstring
    
    * build(ruff): fix ruff config and lint check
    
    * style: fix EOF lint issues
    
    * refactor(location): fix ruff issues

--- src/linceml/__init__.py
+++ src/linceml/__init__.py
@@ -1,3 +1,24 @@
-__version__ = "0.1.0"
+"""LinceML.
 
-__all__ = ["__version__"]
+========================
+
+A biblioteca LinceML é um projeto de framework destinado
+ao monitoramento de qualidade e drift em ambientes de machine
+learning. Desenvolvida com foco em leveza, robustez e
+compatibilidade, a biblioteca é capaz de operar em ambientes
+locais utilizando Polars e ambientes de computação distribuida Spark.
+
+Exemplo básico de uso:
+----------------------
+* TODO Add example usage
+
+Módulos Disponíveis:
+--------------------
+* TODO Add modules list
+
+TODO: Add project URL
+Para a documentação completa, visite: <...>.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Gabriel Camelo"
